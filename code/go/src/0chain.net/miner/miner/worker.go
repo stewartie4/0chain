e22a15e64f858daf99e49f8fccce1dfff8f83349
Kenwes13 <kenwes13@gmail.com>
2018-11-29 17:25:06 -0800

    Update worker.go

--- code/go/src/0chain.net/miner/miner/worker.go
+++ code/go/src/0chain.net/miner/miner/worker.go
@@ -201,7 +201,7 @@ func GenerateClients(numClients int32) {
 	created := 0
 	for _, w := range wallets {
 		//generous airdrop in dev/test mode :)
-		txn := ownerWallet.CreateSendTransaction(w.ClientID, prng.Int63n(10000)*10000000000, "DEBUG")
+		txn := ownerWallet.CreateSendTransaction(w.ClientID, prng.Int63n(10000)*10000000000, "debug")
 		_, err := transaction.PutTransaction(tctx, txn)
 		if err != nil {
 			Logger.Error("air drop to clients", zap.Any("error", err))
