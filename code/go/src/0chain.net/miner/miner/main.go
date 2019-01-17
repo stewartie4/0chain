5304655bdb526ff05377744e355ee545da5876e1
jay-at-0chain <jay@0chain.net>
2019-01-17 15:13:49 -0800

    Revert "cleanup before merge"
    This reverts commit 78d55f0ff6a12c9c71f87441d2a4e647b586f4be.

--- code/go/src/0chain.net/miner/miner/main.go
+++ code/go/src/0chain.net/miner/miner/main.go
@@ -106,6 +106,11 @@ func main() {
 		// node.Self.signatureScheme
 	}
 
+	config.SetServerChainID(config.Configuration.ChainID)
+	common.SetupRootContext(node.GetNodeContext())
+	ctx := common.GetRootContext()
+	initEntities()
+	serverChain := chain.NewChainFromConfig()
 	miner.SetupMinerChain(serverChain)
 	mc := miner.GetMinerChain()
 	mc.DiscoverClients = viper.GetBool("server_chain.client.discover")
@@ -316,4 +321,35 @@ func NewHTTPRequest(method string, url string, data []byte) (*http.Request, cont
 func RegisterMiner(ctx context.Context, chain *chain.Chain) {
 	nodeBytes, _ := json.Marshal(node.Self.Node)
 	SendPostRequestSync(REGISTER_CLIENT, nodeBytes, chain)
+	// time.Sleep(transaction.SLEEP_FOR_TXN_CONFIRMATION * time.Second)
+
+	// txn := transaction.NewTransactionEntity()
+	//txn := "yes"
+
+	// sn := &transaction.StorageNode{}
+	// sn.ID = node.Self.GetKey()
+	// sn.BaseURL = node.Self.GetURLBase()
+
+	// scData := &transaction.SmartContractTxnData{}
+	// scData.Name = transaction.ADD_BLOBBER_SC_NAME
+	// scData.InputArgs = sn
+
+	// txn.ToClientID = transaction.STORAGE_CONTRACT_ADDRESS
+	// txn.Value = 0
+	// txn.TransactionType = transaction.TxnTypeSmartContract
+	// txnBytes, err := json.Marshal(scData)
+	// if err != nil {
+	// 	return "", err
+	// }
+	// txn.TransactionData = string(txnBytes)
+
+	// err = txn.ComputeHashAndSign()
+	// if err != nil {
+	// 	Logger.Info("Signing Failed during registering blobber to the mining network", zap.String("err:", err.Error()))
+	// 	return "", err
+	// }
+	// Logger.Info("Adding blobber to the blockchain.", zap.String("txn", txn.Hash))
+	// transaction.SendTransaction(txn, sp.ServerChain)
+	// return txn.Hash, nil
+	//return txn, nil
 }
