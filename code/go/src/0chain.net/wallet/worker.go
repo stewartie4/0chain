c683f8614cedd673805dd7366e107fc76cca66eb
Kenwes13 <kenwes13@gmail.com>
2019-02-07 15:15:49 -0800

    Updated worker for wallet

--- code/go/src/0chain.net/wallet/worker.go
+++ code/go/src/0chain.net/wallet/worker.go
@@ -8,24 +8,44 @@ import (
 	"go.uber.org/zap"
 )
 
+var successChannel chan bool
+
+func init() {
+	successChannel = make(chan bool, 256)
+}
+
 func SetupWorkers(ctx context.Context, c *Cluster, w *Wallet) {
-	c.RegisterWithMiners(w)
-	for i := 0; i < c.TBWorkers-1; i++ {
-		go c.transactionBlaster(w)
-	}
 	c.transactionBlaster(w)
 }
 
 func (c *Cluster) transactionBlaster(w *Wallet) {
-	count := 0
+	c.RegisterWithMiners(w)
+	for i := 0; i < c.TBWorkers; i++ {
+		go c.transactionSender(w)
+	}
+	c.transactionCounter(w)
+}
+
+func (c *Cluster) transactionSender(w *Wallet) {
 	for true {
 		txn := w.CreateDataTransaction("this is a message at: " + time.Now().String())
 		_, err := c.SendTransaction(txn)
-		if err == nil {
-			count++
-			Logger.Info("sent transaction", zap.Any("count", count))
-		} else {
-			Logger.Error("failed to send transaction", zap.Any("error", err))
+		successChannel <- err == nil
+	}
+}
+
+func (c *Cluster) transactionCounter(w *Wallet) {
+	successCount := 0
+	failureCount := 0
+	for true {
+		select {
+		case success := <-successChannel:
+			if success {
+				successCount++
+			} else {
+				failureCount++
+			}
+			Logger.Info("transaction counter", zap.Any("success", successCount), zap.Any("failure", failureCount))
 		}
 	}
 }
