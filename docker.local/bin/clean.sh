3a4a7efd560a7bdf9cdcf74d638eabf5f666d022
sfxdx <repo@sfxdx.ru>
2020-09-22 11:12:14 +0700

    Run clean script from any location

--- docker.local/bin/clean.sh
+++ docker.local/bin/clean.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
-for i in $(seq 1 3)
+local_dir=$(dirname $0)/..
+
+for i in $(seq 1 7)
 do
   echo "deleting miner$i logs"
-  rm -rf docker.local/miner$i/log/*
+  rm -rf $local_dir/miner$i/log/*
   echo "deleting miner$i redis db"
-  rm -rf docker.local/miner$i/data/redis/state/*
-  rm -rf docker.local/miner$i/data/redis/transactions/*
+  rm -rf $local_dir/miner$i/data/redis/state/*
+  rm -rf $local_dir/miner$i/data/redis/transactions/*
   echo "deleting miner$i rocksdb db"
-  rm -rf docker.local/miner$i/data/rocksdb/*
+  rm -rf $local_dir/miner$i/data/rocksdb/*
 done
 
 for i in $(seq 1 3)
 do
   echo "deleting sharder$i logs"
-  rm -rf docker.local/sharder$i/log/*
+  rm -rf $local_dir/sharder$i/log/*
   echo "deleting sharder$i cassandra db"
-  rm -rf docker.local/sharder$i/data/cassandra/*
+  rm -rf $local_dir/sharder$i/data/cassandra/*
   echo "deleting sharder$i rocksdb db"
-  rm -rf docker.local/sharder$i/data/rocksdb/*
+  rm -rf $local_dir/sharder$i/data/rocksdb/*
 done
 
 for i in $(seq 1 3)
 do
   echo "deleting sharder$i blocks on the file system"
-  rm -rf docker.local/sharder$i/data/blocks/*
+  rm -rf $local_dir/sharder$i/data/blocks/*
 done
